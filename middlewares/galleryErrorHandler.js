a00df5a0c07d4aeb77141ab8e03c96b934e18e52
Jakub Niznik <jakub.niznik@softacus.com>
2024-01-05 22:31:37 +0100

    edited error response

--- middlewares/galleryErrorHandler.js
+++ middlewares/galleryErrorHandler.js
@@ -1,30 +1,17 @@
 const GalleryError = require("./GalleryError");
-const {MulterError} = require("multer");
 
 module.exports = function galleryErrorHandler(err, req, res, next) {
   if (err instanceof GalleryError) {
     console.error(err);
     res.status(err.status).send({
-      error: {
-        status: err.status,
-        message: err.message,
-      },
-    });
-  } else if (err instanceof MulterError) {
-    console.error(err);
-    res.status(400).send({
-      error: {
-        status: 400,
-        message: 'Invalid request - file not found.',
-      },
+      status: "fail",
+      message: err.message
     });
   } else {
     console.error(err);
     res.status(500).send({
-      error: {
-        status: 500,
-        message: 'Internal Server Error',
-      },
+      status: "fail",
+      message: 'Internal Server Error'
     });
   }
 };
